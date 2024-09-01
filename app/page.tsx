2207eb54c530b6b49eeec653cb9e2d3f33466ba4
lisaorg <amol.yadav.778@gmail.com>
2024-09-01 12:59:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import { page_routes } from "@/lib/routes-config";
+import { DATA } from "@/lib/site-config";
 import { MoveUpRightIcon, TerminalIcon } from "lucide-react";
 import Link from "next/link";
 
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
       <Link
-        href="https://github.com/nisabmohd/Documentation-Template"
+        href={DATA.githubUrl}
         target="_blank"
         className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4"
       >
@@ -15,12 +16,10 @@ export default function Home() {
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
       </Link>
       <h1 className="text-3xl font-bold mb-4 sm:text-7xl">
-        An example app built using Next.js and server components.
+        {DATA.siteName}
       </h1>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
-        This feature-packed documentation template, built with Next.js, offers a
-        sleek and responsive design, perfect for all your project documentation
-        needs.
+        {DATA.siteDescription}
       </p>
       <div className="flex flex-row items-center gap-5">
         <Link
@@ -29,19 +28,9 @@ export default function Home() {
         >
           Get Stared
         </Link>
-        <Link
-          href="#"
-          className={buttonVariants({
-            variant: "outline",
-            className: "px-6",
-            size: "lg",
-          })}
-        >
-          Customize
-        </Link>
       </div>
       <span className="flex flex-row items-center gap-2 text-zinc-400 text-md mt-7 -mb-12 max-[800px]:mb-12">
-        <TerminalIcon className="w-4 h-4 mr-1" /> ~ npx this-library-name@latest
+        <TerminalIcon className="w-4 h-4 mr-1" /> {DATA.terminalCommand}
       </span>
     </div>
   );
