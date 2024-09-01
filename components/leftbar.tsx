438ab3369fd8e4909bb9a9982af35ee5d068466d
lisaorg <amol.yadav.778@gmail.com>
2024-09-01 14:36:08 +0530

    Update leftbar.tsx

--- components/leftbar.tsx
+++ components/leftbar.tsx
@@ -6,7 +6,7 @@ import {
   SheetHeader,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Logo, NavMenu } from "./navbar";
+import { NavMenu } from "./navbar";
 import { Button } from "./ui/button";
 import { AlignLeftIcon } from "lucide-react";
 import { DialogTitle } from "./ui/dialog";
@@ -34,7 +34,6 @@ export function SheetLeftbar() {
         <DialogTitle className="sr-only">Menu</DialogTitle>
         <SheetHeader>
           <SheetClose className="px-5" asChild>
-            <Logo />
           </SheetClose>
         </SheetHeader>
         <ScrollArea className="flex flex-col gap-4">
